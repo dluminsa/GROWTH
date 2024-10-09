d1927350da175149d1001603faa120f2d8398673
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-10-09 08:00:19 +0300

    Update EMR_READER.py

--- EMR_READER.py
+++ EMR_READER.py
@@ -1370,8 +1370,9 @@ if st.session_state.reader:# and st.session_state.df:
                                                     file_name=f"{facility} IIT_NEW.csv",
                                                     mime="text/csv")
                             with colb:
-                                    if outnew1==0:
+                                if outnew1==0:
                                         st.markdown('**NO TxNEW TOs**')
+                                else:
                                     dat = yearto1()
                                     csv_data = dat.to_csv(index=False)
                                     st.download_button(key='k',
