3032bc65212e55f8b1605d0fd138befb60296cd3
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-10-09 07:55:39 +0300

    Update EMR_READER.py

--- EMR_READER.py
+++ EMR_READER.py
@@ -1383,4 +1383,4 @@ if st.session_state.reader:# and st.session_state.df:
                             st.write('')
                             st.write('')
                             st.info('**CREATED BY LUMINSA DESIRE**')
-st.write('THEY CAME')
+st.write('THEY CAME'
