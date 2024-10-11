51d1cfdbd14496cd9d12eadcfc2b242a5c588835
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-10-11 19:52:30 +0300

    Update SOPs.py

--- pages/SOPs.py
+++ pages/SOPs.py
@@ -27,56 +27,16 @@ html_table = """
     <th>Purpose: To harmonize the procedure of extraction of patient lists from UgandaEMR using Cohort builder </th>
   </tr>
 </table>
-<table style="width:100%">
-  <tr>
-    <th >EMR COLUMN</th>
-    <th>RENAME TO:</th> 
-    <th>EMR COLUMN</th>
-    <th>RENAME TO:</th>
-  </tr>
- <tr>
-    <td><b>1. HIV Clinic No.</b></td>
-    <td><b>ART</b></td>
-    <td><b>2. ART START DATE</b></td>
-     <td><b>AS</b></td>
- </tr>
- <tr>
-    <td><b>3. TRANSFER OUT DATE</b></td>
-    <td><b>TO</b></td>
-    <td><b>4. Death Date</b></td>
-     <td><b>DD</b></td>
- </tr>
-  <tr>
-    <td><b>5. LAST ENCOUNTER DATE</b></td>
-    <td><b>LD</b></td>
-    <td><b>6. FIRST ENCOUNTER DATE</b></td>
-     <td><b>FE</b></td>
- </tr>
-  <tr>
-    <td><b>7. RETURN VISIT DATE</b></td>
-    <td><b>RD</b></td>
-    <td><b>8. RETURN VISIT DATE1</b></td>
-     <td><b>RD1</b></td>
- </tr>
-  <tr>
-    <td><b>9.RETURN VISIT DATE2</b></td>
-    <td><b>RD2</b></td>
-    <td><b>10.RETURN VISIT DATE_Obs Date</b></td>
-     <td><b>RDO</b></td>
- </tr>
-  <tr>
-    <td><b>11. TRANSFER IN OBS DATE</b></td>
-    <td><b>TI</b></td>
-    <td><b>12. HIV VIRAL LOAD DATE </b></td>
-     <td><b>VD</b></td>
- </tr>
-   <tr>
-    <td><b>13. ARV REGIMEN DAYS DISPENSED</b></td>
-    <td><b>ARVD</b></td>
-    <td><b>14. ARV REGIMEN DAYS DISPENSED_obsDatetime</b></td>
-     <td><b>ARVDO</b></td>
- </tr>
-</table>
+"""
+# Display the HTML table using markdown in Streamlit
+st.markdown(html_table, unsafe_allow_html=True)
+
+# HTML Table
+html_table = """
+<p> 
+Run ever-enrolled list with HIV Clinic no, Gender, Age, Telephone Number, Last Encounter date, First encounter date, Return visit date (check location, check obs datetime, return most recent 3 RVDs), Transferred out to another facility, Transfer In, Current ARV regimen, ART START date, Deceased, ART days dispensed (check obs datetime)
+</p>
+<p>-Filter HIV Clinic No to remove blanks and maintain PLHIV only. (those with a patient ID)</p>
 """
 # Display the HTML table using markdown in Streamlit
 st.markdown(html_table, unsafe_allow_html=True)
