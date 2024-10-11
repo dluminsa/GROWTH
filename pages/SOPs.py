baa5b61cae1f86ee113795d45f368d0c16965389
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-10-11 20:22:43 +0300

    Update SOPs.py

--- pages/SOPs.py
+++ pages/SOPs.py
@@ -50,5 +50,71 @@ html_table = """
   <li>-	Then go to the most recent return visit date and select appointment dates of interest.</li>
 </ul>
 """
-# Display the HTML table using markdown in Streamlit
+st.markdown(html_table, unsafe_allow_html=True)
+html_table = """
+<h4>2.MISSED APPOINTMENT LIST;</h4>
+<p>Repeat the same steps as for the appointment list after ensuring that the EMR is up to date without any backlog.</p>
+<ul>
+  <li>-	In the column of the most recent return visit date, a patient that has a return visit date for the previous period being queried is considered missed.</li>
+  <li>-	For clinics that have community pharmacies using ART access consider locations for ART clinic, Health facility name, and MCH to query missed appointments.</li>
+  </ul>
+"""
+st.markdown(html_table, unsafe_allow_html=True)
+# html_table = """
+# <h4></h4>
+# <p></p>
+# <ul>
+#   <li></li>
+#   <li></li>
+#   </ul>
+# """
+# st.markdown(html_table, unsafe_allow_html=True)
+
+html_table = """
+<h4>3.TX-NEW; (Children and adults new on ART in the reporting period)</h4>
+<ul>
+  <li>-	Using ever enrolled from UgEMR, go to ART start date and consider the dates for the reporting period say Jan-Mar 24. For O-D 23, consider PLHIV with ART start dates btn O – D 23. </li>
+  <li>-	Exclude transfer-ins as they are being reported by the parent facility.</li>
+  </ul>
+"""
+st.markdown(html_table, unsafe_allow_html=True)
+html_table = """
+<h4>4.TX-CURR; (Children and adults active on ART in the reporting period)</h4>
+<p></p>
+<ul>
+  <li>-	From the ever-enrolled extract from UgEMR, remove transfer outs (double check if they are true transfer outs, especially for clients with appointments beyond the reporting period.
+	Filter out dead clients (true from the deceased column).
+</li>
+  <li>-	Go to the most recent return visit date and select dates from 3rd of the last month of the quarter and beyond which makes the TX-CURR. For example, for Jan-Mar 24, all patients with a return visit date of 3rd March 2024 and beyond should make the TX-CURR list. For O-D 23, consider PLHIV with RVD of 3rd Dec 23 and beyond as TX_CURR. </li>
+  <li>-	Check the current ARV regimen and ensure there are no blanks that may be clients active on pre-ART.</li>
+  <li>-	This includes patients on MMD.</li>
+  </ul>
+"""
+st.markdown(html_table, unsafe_allow_html=True)
+
+html_table = """
+<h4>5.	TX-ML, (PLHIV active at the start of the reporting period and had no clinical contact since their last expected date).</h4>
+<ul>
+  <li>-	From the ever enrolled from UgEMR, filter out patients who were active at the start of the reporting period <b><i>and those that initiated ART in the reporting quarter</i></b> but have not returned since their last expected date. Take an example, reporting for Jan-Mar 24, TX-ML list will constitute all patients with a return visit date between 3rd December 2023 to 2nd March 2024. For O-D 23, consider 2nd Sept 23 to 3rd  Dec 2023. </li>
+  <li>-	Leave transfer outs and dead in the ML list.</li>
+  </ul>
+"""
+st.markdown(html_table, unsafe_allow_html=True)
+
+type="1"
+st.markdown(html_table, unsafe_allow_html=True)
+html_table = """
+<h4>6.	TX-RTT, (Patients who returned to care after experiencing an IIT for more than 28 days and remained active until the end of the quarter).</h4>
+<p></p>
+<ol type='a'>
+  <li>-	From the confirmed TX-CURR list, go to last encounter date and select clients with a last clinical visit in the reporting quarter.</li>
+  <li>-	Go to the column of first encounter date and remove dates for the reporting quarter.</li>
+  <li>-	Go to ART start date and remove newly enrolled clients on ART in the reporting quarter.</li>
+  <li><b><i>-Go to Transfer In and remove transfer Ins in the reporting qtr.</i></b></li>
+  <li>-	Go to the obs datetime of the most recent return visit date, remove/eliminate obs datetime that falls in the previous reporting periods.</li>
+  <li>-	Go to the second most recent return visit date and remove dates from 2nd or 3rd of the third month of the previous reporting qtr, to the reporting quarter and beyond. </li>
+  <li><b><i>Go to the third most recent return visit date and remove dates from 2nd or 3rd of the third month of the previous reporting qtr, to the reporting quarter and beyond.</i></b></li>
+  <li><b><u>-	Go to the ART days dispensed obs datetime and remove all clients that didn’t receive ART in the reporting quarter.</u>What remains is the TX-RTT.</b></li>
+  </ol>
+"""
 st.markdown(html_table, unsafe_allow_html=True)
