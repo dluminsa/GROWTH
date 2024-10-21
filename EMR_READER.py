45f7c5804786a970fd1fbef088c395bca6cba0b4
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-10-21 21:50:34 +0300

    Update EMR_READER.py

--- EMR_READER.py
+++ EMR_READER.py
@@ -225,6 +225,8 @@ def extract():
                 
                         A[['Ryear', 'Rmonth', 'Rday']] = A['RD'].str.split('-', expand = True)
                         B[['Ryear', 'Rmonth', 'Rday']] = B['RD'].str.split('/', expand = True)
+                        st.write('GONE')
+                        st.write(D['RD'])
                         try:
                             D['RD'] = D['RD'].astype(int)
                             #D['RD'] = pd.to_numeric(D['RD'], errors='coerce')
