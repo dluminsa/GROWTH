1b5b29465e3e90b289b21b4790e537ba3edae4df
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-10-27 09:05:52 +0300

    Update EMR_READER.py

--- EMR_READER.py
+++ EMR_READER.py
@@ -678,7 +678,7 @@ def extract():
                         df['RWEEKR'] = pd.to_numeric(df['RWEEKR'], errors='coerce')
                         df['RWEEKR1'] = df['RWEEKR']-39 #NOT NEEDED THIS Q SINCE WE ARE USING 
         
-                        df['DURA'] = round((df['RETURN DATE1'] - df['LAST DATE']).dt.days / 30)
+                        df['DURA'] = round((df['LAST DATE']-df['RETURN DATE1']).dt.days / 30)
         
                         today = datetime.now()
                         todayr = pd.to_datetime(today)
