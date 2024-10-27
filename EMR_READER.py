282675f834198fc27f6b8dd3ef5c4ec812292b9c
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-10-27 07:14:05 +0300

    Update EMR_READER.py

--- EMR_READER.py
+++ EMR_READER.py
@@ -686,7 +686,7 @@ def extract():
 
                         # Calculate the difference in months
                         df['DURL'] = round((todayr - df['RETURN DATE']).dt.days / 30)
-                        aa = df[['RETURN DATE','DURL']]
+                        aa = df[['RETURN DATE','DURA']]
                         st.write(aa)
                         def cira(a):
                             if a<1:
