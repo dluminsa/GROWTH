8c0df1f96b2db465c36e8f6715a57b9df4c12007
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-04 11:49:37 +0300

    Update EMR_READER.py

--- EMR_READER.py
+++ EMR_READER.py
@@ -1528,7 +1528,7 @@ def extract():
                     ns['ART'] = pd.to_numeric(ns['ART'], errors='coerce')
                     nsps['ART'] = pd.to_numeric(nsps['ART'], errors='coerce')
                     jj = nsps[nsps['ART']==7718]
-                    st.write(jj)
+                    st.write(nsps.shape[0])
                     allns = pd.merge(ns, nsps, on ='ART', how='left')
                     allns = allns[['DISTRICT', 'facility','ART','result_numeric', 'date_collected', 'AG', 'Ryear', 'Rmonth', 'Rday', 'RWEEKR','VD','TO','DD']]
                     
