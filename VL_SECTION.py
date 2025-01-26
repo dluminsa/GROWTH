d1d62d27245d2378e04077ed94a6e6b90d662ae9
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-26 06:54:37 +0300

    Update VL_SECTION.py

--- VL_SECTION.py
+++ VL_SECTION.py
@@ -393,7 +393,53 @@ for fac in facilities:
      cold.write(f'**{int(ja)}**')
      cole.write(f'**{fe:,.0f}**')
      colf.write(f'**{mar:,.0f}**')
+st.divider()
+st.markdown('<p><b><u><i style="color:magenta">VIRAL LOAD (CLIENTS DUE FOR BLEEDING)</i></u></b></p>' , unsafe_allow_html = True)
 
+vl = dfline[['CLUSTER', 'DISTRICT', 'FACILITY', 'A', 'RD', 'Rmonth', 'Rday', 'VL STATUS', 'RWEEK', 'USE']].copy()
+vl= vl[vl['vl STATUS'].notna()].copy()
+vlsumvl = dfsum[['CLUSTER', 'DISTRICT', 'FACILITY','JANVL', 'FEBVL','MARVL', 'WEEK']].copy()
+vlsumvl = vlsumvl[vlsumvl['WEEK']==wiki].copy()
+
+cola, colb, colc, cold, cole, colf = st.columns([2,1,1,1,1,1])
+cola.write(f'**{word}**')
+colb.write('**TODAY**')
+colc.write('**THIS WEEK**')
+cold.write('**JAN**')
+cole.write('**FEB**')
+colf.write('**MAR**')
+for fac in facilities:
+     vl = vl[vl['USE'] == fac]
+     vl[['Rmonth', 'Rday', 'RWEEK']] = vl[['Rmonth', 'Rday','RWEEK']].apply(pd.to_numeric, errors='coerce')
+     tod = vl[((vl['Rmonth'] == mon) & (vl['Rday'] == today))].copy()
+     tods = tod.shape[0]
+     wik = vl[(vl['RWEEK'] == wiki)].copy()
+     wikis = wik.shape[0]
+     vlsumx = dfsum[dfsum['USE']==fac].copy()
+     try:
+       jansumvl = vlsumvl['JANVL'].sum()
+     except:
+          jansumvl= 0
+     try:
+          febsumvl = vlsumvl['FEBVL'].sum()
+     except:
+          febsumx = 0
+     try:
+          marsumvl = vlsum['MARVL'].sum()
+     except:
+          marsumvl = 0
+          
+     cola.write(f'**{fac}**')
+     colb.write(f'**{tods:,.0f}**')
+     colc.write(f'**{wikis:,.0f}**')
+     cold.write(f'**{int(jansumvl)}**')
+     cole.write(f'**{febsumvl:,.0f}**')
+     colf.write(f'**{marsumvl:,.0f}**')
+
+
+
+
+st.divider()
 HAVE = water['HAVE'].sum()
 NOT = water['NOVL'].sum()
 TOTAL = int(HAVE) + int(NOT)
