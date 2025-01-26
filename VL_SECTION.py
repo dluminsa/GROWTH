65a0538645e163a9bf9607f3352edf379687ec11
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-26 07:11:32 +0300

    Update VL_SECTION.py

--- VL_SECTION.py
+++ VL_SECTION.py
@@ -393,6 +393,7 @@ for fac in facilities:
      cole.write(f'**{fe:,.0f}**')
      colf.write(f'**{mar:,.0f}**')
 st.divider()
+
 st.markdown('<p><b><u><i style="color:magenta">VIRAL LOAD (CLIENTS DUE FOR BLEEDING)</i></u></b></p>' , unsafe_allow_html = True)
 
 vl = dfline[['CLUSTER', 'DISTRICT', 'FACILITY', 'A', 'RD', 'Rmonth', 'Rday', 'VL STATUS', 'RWEEK', 'USE']].copy()
@@ -434,7 +435,40 @@ for fac in facilities:
      cold.write(f'**{int(jansumvl)}**')
      cole.write(f'**{febsumvl:,.0f}**')
      colf.write(f'**{marsumvl:,.0f}**')
+st.divider()
+st.markdown('<p><b><u><i style="color:magenta">TWO MONTHS BLEEDING WINDOW (CLIENTS DUE IN TWO MONTHS)</i></u></b></p>' , unsafe_allow_html = True)
 
+vl = dfline[['CLUSTER', 'DISTRICT', 'FACILITY', 'A', 'RD', 'Rmonth', 'Rday', 'TWOm', 'RWEEK', 'USE']].copy()
+vl= vl[vl['TWOm'].notna()].copy()
+
+cola, colb, colc, cold, cole, colf = st.columns([2,1,1,1,1,1])
+cola.write(f'**{word}**')
+colb.write('**TODAY**')
+colc.write('**THIS WEEK**')
+cold.write('**JAN**')
+cole.write('**FEB**')
+colf.write('**MAR**')
+for fac in facilities:
+     vl = vl[vl['USE'] == fac]
+     vl[['Rmonth', 'Rday', 'RWEEK']] = vl[['Rmonth', 'Rday','RWEEK']].apply(pd.to_numeric, errors='coerce')
+     tod = vl[((vl['Rmonth'] == mon) & (vl['Rday'] == today))].copy()
+     tods = tod.shape[0]
+     wik = vl[(vl['RWEEK'] == wiki)].copy()
+     wikis = wik.shape[0]
+     vl[['Rmonth', 'Rday', 'RWEEK']] = vl[['Rmonth', 'Rday','RWEEK']].apply(pd.to_numeric, errors='coerce')
+     janvl = vl[((vl['Ryear'] == 2025) & (vl['Rmonth'] == 1))].copy() #ALTER THESE
+     febvl = vl[((vl['Ryear'] == 2025) & (vl['Rmonth'] == 2))].copy()
+     marvl = vl[((vl['Ryear'] == 2025) & (vl['Rmonth'] == 3))].copy()
+     jansumvl = janvl.shape[0]
+     febsumvl = febvl.shape[0]
+     febsumvl = marvl.shape[0]
+     
+     cola.write(f'**{fac}**')
+     colb.write(f'**{tods:,.0f}**')
+     colc.write(f'**{wikis:,.0f}**')
+     cold.write(f'**{int(jansumvl)}**')
+     cole.write(f'**{febsumvl:,.0f}**')
+     colf.write(f'**{marsumvl:,.0f}**')
 
 
 
