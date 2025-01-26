b2af0b939c09b8b491d26311ede2d0f95d1ccfc8
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-01-26 06:36:24 +0300

    Update VL_SECTION.py

--- VL_SECTION.py
+++ VL_SECTION.py
@@ -46,14 +46,14 @@ if 'ns' not in st.session_state:
      try:
         #cola,colb= st.columns(2)
         conn = st.connection('gsheets', type=GSheetsConnection)
-        exist = conn.read(worksheet= 'ALLNS', usecols=list(range(12)),ttl=5)
+        exist = conn.read(worksheet= 'ALLNS', usecols=list(range(23)),ttl=5)
         txa = exist.dropna(how='all')
         st.session_state.ns = txa
      except:
          st.write("POOR NETWORK, COULDN'T CONNECT TO DATABASE")
          st.stop()
 dfns = st.session_state.ns.copy()
-st.write(dfns)
+
 if 'line' not in st.session_state:     
      try:
         #cola,colb= st.columns(2)
