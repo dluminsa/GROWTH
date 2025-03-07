7c8f4e5c07cf7ba324be0b87c4d9a02308d21f82
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-03-07 14:28:01 +0300

    Update EMR_READER.py

--- EMR_READER.py
+++ EMR_READER.py
@@ -851,8 +851,7 @@ def extract():
                         cur25[['Ryear', 'Rmonth', 'Rday']] = curlostb[['Ryear', 'Rmonth', 'Rday']].apply(pd.to_numeric, errors ='coerce')
                         cur25 = cur25[ ((cur25['Rmonth']>3) |(( cur25['Rmonth']==3) & (curlostb['Rday']>3)))].copy()
                         dfcur = pd.concat([cur25, cur26])
-                        st.write('THIS')
-                        st.write(dfcur.shape[0])
+
                         #VL SECTION 
                         dfcur[['Vyear', 'Vmonth']] = dfcur[['Vyear', 'Vmonth']].apply(pd.to_numeric, errors='coerce')
                         
