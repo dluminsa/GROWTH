924ef2c8c21476a1bcd4167bfaad04c1dc0e78c1
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-03-11 18:23:03 +0300

    Update EMR_READER.py

--- EMR_READER.py
+++ EMR_READER.py
@@ -5,6 +5,7 @@ import numpy as np
 import gspread
 from openpyxl import load_workbook
 from pathlib import Path
+SSSS
 import traceback
 import time
 from google.oauth2.service_account import Credentials
