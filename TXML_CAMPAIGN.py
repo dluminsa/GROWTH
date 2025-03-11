a693edeac207f12f66481e8ae81cfb9923a015bd
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-03-11 09:31:04 +0300

    Update TXML_CAMPAIGN.py

--- TXML_CAMPAIGN.py
+++ TXML_CAMPAIGN.py
@@ -32,17 +32,17 @@ dd = int(week)
 k = int(wk)
 #st.warning('***CURRENT DATA IS FOR DEMONSTRATION ONLY, WILL BE REMOVED AFTER ROLLING OUT THE DASHBOARD**')
 
-# if 'tx' not in st.session_state:     
-#      try:
-#         #cola,colb= st.columns(2)
-#         conn = st.connection('gsheets', type=GSheetsConnection)
-#         exist = conn.read(worksheet= 'TX', usecols=list(range(22)),ttl=5)
-#         tx = exist.dropna(how='all')
-#         st.session_state.tx = tx
-#      except:
-#          st.write("POOR NETWORK, COULDN'T CONNECT TO DELIVERY DATABASE")
-#          st.stop()
-# dftx = st.session_state.tx.copy()
+if 'tx' not in st.session_state:     
+     try:
+        #cola,colb= st.columns(2)
+        conn = st.connection('gsheets', type=GSheetsConnection)
+        exist = conn.read(worksheet= 'TX', usecols=list(range(22)),ttl=5)
+        tx = exist.dropna(how='all')
+        st.session_state.tx = tx
+     except:
+         st.write("POOR NETWORK, COULDN'T CONNECT TO DELIVERY DATABASE")
+         st.stop()
+dftx = st.session_state.tx.copy()
 # #st.write(dftx)
 
 if 'q4' not in st.session_state:     
